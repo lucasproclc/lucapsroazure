f1ba3f2ac49f685e870edd97026bb384b0b63029
lucasproclc <lucaspro.dev@gmail.com>
2018-06-19 04:49:32 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,8 +4,8 @@ variable "resourcename" {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-    subscription_id = "cc786216-c843-4350-ab79-a01e805af94c"
-    client_id       = "d83e297a-2cff-4a8d-811b-b108451a5ce2"
+    subscription_id = "11"
+    client_id       = "11"
     client_secret   = "Y8y6Ha4xmz4Z9qanogE9H14y8icOkaICGmf5tijCWB8="
     tenant_id       = "e822651b-5027-4253-83f5-904854601a3b"
 }
