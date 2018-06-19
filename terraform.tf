c7881ce0543667e1d876da31de2ebdb855388a58
lucasproclc <lucaspro.dev@gmail.com>
2018-06-19 00:02:29 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 variable "resourcename" {
-  default = "TRIANZSCALRGROUP"
+  default = "myResourceGroup"
 }
 
 # Configure the Microsoft Azure Provider
@@ -12,7 +12,7 @@ provider "azurerm" {
 
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "TRIANZSCALRGROUP"
+    name     = "myResourceGroup"
     location = "eastus"
 
     tags {
@@ -111,6 +111,51 @@ resource "azurerm_storage_account" "mystorageaccount" {
     location                    = "eastus"
     account_tier                = "Standard"
     account_replication_type    = "LRS"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "eastus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "myvm"
+        admin_username = "azureuser"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
+        }
+    }
+
+    boot_diagnostics {
+        enabled = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
 
     tags {
         environment = "Terraform Demo"
